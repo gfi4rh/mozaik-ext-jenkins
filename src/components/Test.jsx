5776ae6c69b8420da56a2475fdcb17e7665e3d7d
Regourd Colin <colin.regourd@inetum.world>
2021-06-01 18:29:25 +0200

    bar chart

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -7,7 +7,9 @@ class Test extends Component {
 
   constructor(props){
     super(props)
-    this.state = {} 
+    this.state = {
+      stat = null
+    } 
   }
 
 
@@ -24,22 +26,30 @@ class Test extends Component {
     };
   }
   
-  onApiData(test) {
-    console.log(test)
-    /*this.setState({
-      test : test.statistic
-    });*/
+  onApiData(data) {
+    console.log(data)
+    this.setState({
+      stat : data.statistic
+    });
   }
   
   render() {
 
     /*stat.passed, stat.skipped, stat.failed*/
     const { title } = this.props;
+    const { stat } = this.state;
   
     
     return (
       <div className="jenkins__test__line">
 				<div className="jenkins__test__name">{title}</div>
+        { stat && 
+          <div className="jenkins__test__container">
+            <div className="jenkins__test__passed" title="Passed" style={flex-grow : stat.passed}></div>
+            <div className="jenkins__test__skipped" title="Skipped" style={flex-grow : stat.skipped}></div>
+            <div className="jenkins__test__failed" title="Failed" style={flex-grow : stat.failed}></div>
+          </div>
+        }
 			</div>
     );
     }
